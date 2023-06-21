de54c6e38479b703f9cb138efc48f7daa60bc8bb
saeyeun <129368951+saeyeun@users.noreply.github.com>
2023-06-21 16:05:53 +0900

    0620

---
+++ sequence/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    // 누적되는 변수 지정
+    let imgs = ''; 
+    
+    for(var i = 0; i<200; i++){
+
+        imgs+='<img src="img/pic'+i+'.jpg" alt="">'
+        
+        
+        
+    }
+    $('section').html(imgs);
+
+
+    //마우스를 움직일 때 마다 한장씩 사진이 보여라
+    $('section').mousemove(function(e){
+        let x = e.pageX
+        let wd = $(window).width()
+
+        let percent = Math.floor((x/wd)*200)
+
+        $('h1').text(percent)
+        $('section>img').hide();
+        $('section>img').eq(percent).show()
+
+    })
+})
